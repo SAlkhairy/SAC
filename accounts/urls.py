2e813e8df805d5c75955d8a49fc4a00176b7b9ad
SAlkhairy <salkhairy@hotmail.com>
2017-03-21 21:02:11 +0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,1 +1,7 @@
-from django.conf.urls import urlfrom userena import views as userena_viewsfrom forms import CustomSignupFormurlpatterns = [    url(r'^signup/$', userena_views.signup, {'signup_form': CustomSignupForm, 'template_name': 'signup.html'}, name="signup"),    #url(r'^signup/$', userena_views.signup,{'signup_form': CustomSignupForm}),]
+from django.conf.urls import url
+from userena import views as userena_views
+from forms import CustomSignupForm
+
+urlpatterns = [
+  url(r'^signup/$', userena_views.signup, {'signup_form': CustomSignupForm, 'template_name': 'signup.html'}, name="signup"),
+  #url(r'^signup/$', userena_views.signup,{'signup_form': CustomSignupForm}),]
