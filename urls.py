c2647a9a9cbbf57943eed43e7f53845699d16f44
SAlkhairy <salkhairy@hotmail.com>
2017-03-21 18:30:13 +0300

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from userena import views as userena_views
-from forms import CustomSignupForm
-
-urlpatterns = [
-    #url(r'^signup/$', userena_views.signup, {'template_name': 'signup.html'}, name="signup"),
-    url(r'^signup/$', userena_views.signup,{'signup_form': CustomSignupForm}),
-
-]
