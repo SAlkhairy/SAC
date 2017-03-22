bd93676c5554f1bd6262876b667406ec885c1020
U-LEVO\Saleha <salkhairy@hotmail.com>
2017-03-22 17:31:33 +0300

    Voting's Models

--- voting/models.py
+++ voting/models.py
@@ -1,57 +1,12 @@
 # -*- coding: utf-8  -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
 
+from accounts.models import Profile, College
+
 # Create your models here.
-
-
-
-
-gender_choices = (
-    ('F', u'طالبة'),
-    ('M', u'طالب'),
-)
-
-
-
-college_choices = (
-    ('M', u'كلية الطب'),
-    ('A', u'كلية العلوم الطبية التطبيقية'),
-    ('P', u'كلية الصيدلة'),
-    ('D', u'كلية طب الأسنان'),
-    ('B', u'كلية العلوم و المهن الصحية'),
-    ('N', u'كلية التمريض'),
-    ('I', u' كلية الصحة العامة والمعلوماتية الصحية'),
-)
-
-
-
-
-city_choices = (
-    ('R', u'الرياض'),
-    ('J', u'جدة'),
-    ('A', u'الأحساء'),
-)
-
-
-
-class College(models.Model):
-    name = models.CharField(max_length=1, choices=college_choices, verbose_name=u"الاسم")
-    city = models.CharField(max_length=1, choices=city_choices, verbose_name=u"المدينة")
-    gender = models.CharField(max_length=1, choices=gender_choices, verbose_name=u"الجنس")
-
-    def __unicode__(self):
-        return u"%s (%s - %s)" % (self.get_name_display(),
-                                       self.get_section_display(),
-                                       self.get_gender_display())
-
-    class Meta:
-        # For the admin interface.
-        verbose_name = u"كلية"
-        verbose_name_plural = u"الكليات"
-
-
 
 
 
@@ -64,42 +19,46 @@ class SACYear(models.Model):
     election_vote_end_datetime = models.DateTimeField(u"تاريخ نهاية التصويت")
 
 
-
 class Position(models.Model):
-    title = models.CharField()
-    colleges_allowed_to_vote=
-    colleges_allowed_to_nomiate =
-    submission_date = models.DateTimeField(u"تاريخ ال", auto_now_add=True)
-    modification_date = models.DateTimeField(auto_now=True, null=True)
-    year = models.CharField()
+    title = models.CharField(u"اسم المنصب")
+    colleges_allowed_to_vote = models.ManyToManyField(College, u"الكليات المسموحة بالتصويت")
+    colleges_allowed_to_nominate = models.ManyToManyField(College, u"الكليات المسموحة بالترشيح")
+    submission_date = models.DateTimeField(u"تاريخ التقديم", auto_now_add=True)
+    modification_date = models.DateTimeField(u"تاريخ التعديل", auto_now=True, null=True)
+    year = models.CharField(u"السنة", max_length=4)
 
 
-###class Nomiation(models.Model):
-    plan=()
-    CV=()
-    User=()
-    Position=()
-    submission_date = models.DateTimeField(u"تاريخ ال", auto_now_add=True)
-    modification_date = models.DateTimeField(auto_now=True, null=True)
+class Nomination(models.Model):
+    plan = models.FileField(u"الخطة") #right?
+    cv = models.FileField(u"السيرة الذاتية") #right?
+    user = models.OneToOneField(Profile, u"المرشِّح")
+    position = models.ForeignKey(Position, u"المنصب")
+    submission_date = models.DateTimeField(u"تاريخ التقديم", auto_now_add=True)
+    modification_date = models.DateTimeField(u"تاريخ التعديل", auto_now=True, null=True)
 
-class VoteNomition(models.Model):
-    user=()
-    Nomition=()
-    submission_date = models.DateTimeField(u"تاريخ ال", auto_now_add=True)
-    modification_date = models.DateTimeField(auto_now=True, null=True)
 
-class VoteReferendum (models.Model):
-    User=()
-    Referendum=()
-    submission_date = models.DateTimeField(u"تاريخ ال", auto_now_add=True)
-    modification_date = models.DateTimeField(auto_now=True, null=True)
+class VoteNomination(models.Model):
+    user = models.OneToOneField(Profile, u"المصوِّت")
+    nomination = models.ForeignKey(Nomination, u"المرشّح")
+    submission_date = models.DateTimeField(u"تاريخ التقديم", auto_now_add=True)
+    modification_date = models.DateTimeField(u"تاريخ التعديل", auto_now=True, null=True)
+
+
 class Referendum(models.Model):
-    year=()
-    title=()
-    description=()
-    opening_datetime=()
-    closing_datetime=()
-    colleges_allowed=('M2M')
+    year = models.CharField(u"السنة",)
+    title = models.CharField(u"الموضوع", max_length=100)
+    description = models.TextField(u"الوصف", )
+    opening_datetime = models.DateTimeField(u"تاريخ الابتداء",)
+    closing_datetime = models.DateTimeField(u"تاريخ الانتهاء",)
+    colleges_allowed = models.ManyToManyField(College, u"الكليات المسموحة")
+
 
 class ReferendumFigure(models.Model):
-    referendum=()
+    referendum = models.OneToOneField(Referendum, u"الاستفتاء",)
+
+
+class VoteReferendum (models.Model):
+    user = models.OneToOneField(Profile, u"المصوٍّت",)
+    referendum = models.ForeignKey(Referendum, u"الاستفتاء")
+    submission_date = models.DateTimeField(u"تاريخ التقديم", auto_now_add=True)
+    modification_date = models.DateTimeField(u"تاريخ التعديل", auto_now=True, null=True)
