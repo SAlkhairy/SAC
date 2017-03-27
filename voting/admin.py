302431ca35215a868e2b7d1f4c2e584dfc08fce8
U-LEVO\Saleha <salkhairy@hotmail.com>
2017-03-27 18:11:49 +0300

    d Add nomination to admin

--- voting/admin.py
+++ voting/admin.py
@@ -1,11 +1,29 @@
+# -*- coding: utf-8  -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-from voting.models import Position, SACYear
+from voting.models import Position, SACYear, Nomination
 
 # Register your models here.
 
 admin.site.register(SACYear)
 
 admin.site.register(Position)
+
+
+
+def make_rejected(ModelAdmin, request, queryset):
+    queryset.update(is_rejected=True)
+make_rejected.short_description = "رفض المرشحـ/ين المختار/ين"
+
+
+class NominationAdmin(admin.ModelAdmin):
+    list_filter = ['position', 'is_rejected']
+    search_fields = ['position', 'is_rejected']
+    actions = [make_rejected]
+
+admin.site.register(Nomination, NominationAdmin)
+
 
 
