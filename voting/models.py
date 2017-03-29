613f7b31856024619f74a8a40bd39b6d723d5675
U-LEVO\Saleha <salkhairy@hotmail.com>
2017-03-29 15:39:29 +0300

    Certificates Field Added

--- voting/models.py
+++ voting/models.py
@@ -53,6 +53,7 @@ class Position(models.Model):
 class Nomination(models.Model):
     plan = models.FileField(verbose_name="الخطة")
     cv = models.FileField(verbose_name="السيرة الذاتية")
+    certificates = models.FileField(null=True, verbose_name="الشهادات والمساهمات")
     user = models.ForeignKey(User, verbose_name="المرشَّح")
     position = models.ForeignKey(Position, verbose_name="المنصب")
     is_rejected = models.BooleanField(default=False)
