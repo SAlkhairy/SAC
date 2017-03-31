b2b5a2ef905bdf57ae6e623816a822979fcc2bef
U-LEVO\Saleha <salkhairy@hotmail.com>
2017-04-01 01:21:48 +0300

    GPA field added

--- voting/models.py
+++ voting/models.py
@@ -54,7 +54,7 @@ class Nomination(models.Model):
     plan = models.FileField(verbose_name="الخطة")
     cv = models.FileField(verbose_name="السيرة الذاتية")
     certificates = models.FileField(null=True, verbose_name="الشهادات والمساهمات")
-    gpa = models.CharField(verbose_name="المعدل الجامعي", max_length=10, null=False, default="غير مزوّد")
+    gpa = models.CharField(verbose_name="المعدل الجامعي", max_length=100, null=False, default="غير مزوّد")
     user = models.ForeignKey(User, verbose_name="المرشَّح")
     position = models.ForeignKey(Position, verbose_name="المنصب")
     is_rejected = models.BooleanField(default=False)
