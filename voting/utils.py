bc51c014011b5f160c37e2321b4567dbecd49ce2
U-LEVO\Saleha <salkhairy@hotmail.com>
2017-05-06 15:03:01 +0300

    Basic voting and ticket done! Thank God

--- voting/utils.py
+++ voting/utils.py
@@ -5,5 +5,5 @@ import qrcode.image.svg
 def get_ticket(user):
     qrcode_output = StringIO.StringIO()
     qrcode.make(user.pk, image_factory=qrcode.image.svg.SvgImage, version=3).save(qrcode_output)
-    qrcode_value = "".join(qrcode_output.getvalue().split('\n')[1:])
+    qrcode_value = str(user.pk).join(qrcode_output.getvalue().split('\n')[1:])
     return qrcode_value
