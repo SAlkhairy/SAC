62536184f9cf6d58dfabdbdd1df1374d2dd1f3f6
U-LEVO\Saleha <salkhairy@hotmail.com>
2017-11-17 11:58:37 +0300

    Re-adding news_show code

--- general/urls.py
+++ general/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^static/(?P<page_id>\w+)/$', general_views.show_page, name='show_page'),
     url(r'^add_feedback/$', general_views.add_feedback, name='add_feedback'),
     url(r'^news/$', general_views.list_news, name='list_news'),
-#    url(r'^show_news/(?P<news_id>\d+)/$', general_views.show_newsitem, name='show_news'),
+    url(r'^show_news/(?P<news_id>\d+)/$', general_views.show_newsitem, name='show_news'),
 ]
